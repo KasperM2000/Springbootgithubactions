bc8a4d1a544efbaeb0ebec258ebf4b57ff06b8e0
KasperM2000 <kama0006@stud.kea.dk>
2024-09-25 09:27:46 +0200

    initial commit

---
+++ src/main/java/org/example/springbootgithubactions/SpringbootgithubactionsApplication.java
@@ -0,0 +1,13 @@
+package org.example.springbootgithubactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootgithubactionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootgithubactionsApplication.class, args);
+    }
+
+}
